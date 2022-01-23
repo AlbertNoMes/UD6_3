9ec5d5ba60a331d2cd561852a265f419d9d5c357
AlbertNoMes <albertnotariomestres@gmail.com>
2022-01-23 13:33:48 +0100

    FINAL

--- src/NumerosPrimosAPP.java
+++ src/NumerosPrimosAPP.java
@@ -1,29 +1,26 @@
 import javax.swing.JOptionPane;
+
 public class NumerosPrimosAPP {
 
 	public static void main(String[] args) {
-		String texto=JOptionPane.showInputDialog("Introduce un numero");
-		int numero=Integer.parseInt(texto);
-		if (primo(numero)) {
-			System.out.println("El numero "+numero+" es primo");
-		}else {
-			System.out.println("El numero "+numero+" NO es primo");
-		}
+
+		// ABREVIA EL PARSE INT
+		int numero1 = Integer.parseInt(JOptionPane.showInputDialog("Escribe un primer numero"));
+
+		System.out.println(comprovarPrimos(numero1));
 	}
-	public static boolean primo (int numero) {
-		if (numero<=1) {
+
+	public static boolean comprovarPrimos(int numero1) {
+		if (numero1 <= 1) {
 			return false;
 		}
-		int cont=0;
-		for (int divisor=(int)Math.sqrt(numero);divisor>1;divisor--) {
-			if (numero%divisor==0) {
-				cont+=1;
+		for (int i = 2; i <= (numero1) / 2; i++) { // I = 2 XK X 1 TODOS SE DIVIDEN Y / 2 X SI LA MITAD DA DECIMAL
+			if (numero1 % i == 0) {
+				return false; // RETURN = APAGA Y VAMONOS
 			}
+
 		}
-		if (cont>=1) {
-			return false;
-		}else {
-			return true;
-		}
+		return true;
 	}
+
 }
